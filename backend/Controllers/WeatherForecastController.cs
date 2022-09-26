814b8a1e664a8f86be4f6cdf2dce6362abd021dc
Your Namesosominus <elliotness_@hotmail.com>
2022-09-26 18:57:15 +0200

    tests

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace prid_tuto.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
